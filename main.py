3cea85d9f2d341fea3f6172ec149215f3d87e627
SiriRise <119751739+SiriRise@users.noreply.github.com>
2023-02-21 02:47:06 +0400

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,25 @@ import re
 slovar = {"0": "ноль", "1": "один", "2": "два", "3": "три", "4": "четыре", "5": "пять", "6": "шесть", "7": "семь"}
 
 file = open("text.txt", "r")
-good = 0
 while True:
     buffer = file.readline().split()
     if not buffer:
         print("Файл в директории проекта закончился")
         break
+    good = 0
     print("Введите количество разных цифр")
     diff_number = int(input())
     for j in buffer:
         reg = re.findall(r"[0-7]*[0-7]", j)
         if len(reg) == 1:
             if len(j) == len(reg[0]) and len(j) <= 4:
-                if int(j) % 2 != 0 and len(set(str(j))) >= diff_number and int(j, 8) <= 2048:
+                if int(j) % 2 != 0 and len(set(j)) >= diff_number and int(j, 8) <= 2048:
                     good += 1
                     print(j)
                     num = ("".join(sorted(set(j), key=j.index)))
                     for i in range(len(num)):
                         print(slovar[(num[i])], end=" ")
                     print()
-if good == 0:
-    print("нет подходящих чисел")
-
+    if good == 0:
+        print("нет подходящих чисел")
+        break
