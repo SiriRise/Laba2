cbe069d9a0c707ad0e172d70258770778ef5eeb2
SiriRise <119751739+SiriRise@users.noreply.github.com>
2023-03-10 14:52:08 +0400

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ while True:
     for j in buffer:
         reg = re.findall(r'^-?[0-7]*[1357]$', j)
 
-        if len(reg) == 1 and int(j) <= 4000 and len(j) >= 2:
+        if len(reg) == 1 and int(j) <= 4000 and len("".join(j)) >= 2:
                 good = 1
                 print(''.join(reg))
                 num = ("".join(sorted(set(j), key=j.index)))
